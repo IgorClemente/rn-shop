d8b550dd31c98e93a336669d747acb81e6e39f4f
Igor Clemente <igor.clm20@gmail.com>
2024-12-09 22:59:26 +0000

    Orders page updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -166,6 +166,30 @@ export const createOrderItem = () => {
             );
 
             if (error) throw new Error('An error occurred while creating order item: ' + error.message);
+
+            return data;
         }
     });
 }
+
+export const getMyOrder = (slug: string) => {
+    const {
+        user: { id }
+    } = useAuth();
+
+    return useQuery({
+        queryKey: ['order', slug],
+        queryFn: async () => {
+            const { data, error } = await supabase
+                .from('order')
+                .select('*, order_items:order_item(*,products:product(*))')
+                .eq('slug', slug)
+                .eq('user', id)
+                .single();
+
+            if (error || !data) throw new Error('An error occurred while fetching data: ' + error.message);
+
+            return data;
+        }
+    });
+};
